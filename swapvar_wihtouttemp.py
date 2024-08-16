dbc5bee73e9603f56da52be4e50420ce02ecccf7
Shahsaad725 <shahsaaddotani@gmail.com>
2024-08-15 18:17:21 -0700

    Add files via upload

---
+++ swapvar_wihtouttemp.py
@@ -0,0 +1,11 @@
+x= 5
+y= 10
+count = 0
+print("the value of x :",x,"the value of y :",y)
+while x!=y:
+    x=x+1
+    count = count+1
+
+y=y-count
+
+print("the value of x :",x,"the value of y :",y)
